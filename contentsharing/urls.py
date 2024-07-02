bb7f40e8b52de01c1818f86771191fa0436276df
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-07-02 17:33:18 +0000

    fixt the content

--- contentsharing/urls.py
+++ contentsharing/urls.py
@@ -2,10 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from .views import root_route
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
