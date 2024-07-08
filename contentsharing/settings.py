4ffc96f471b58b60ef9818f72a0975eb30f56cfd
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-07-08 16:21:48 +0000

    added debug true

--- contentsharing/settings.py
+++ contentsharing/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
-
+#DEBUG = 'DEV' in os.environ
+DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     'contentsharing-api-7b3cd872bc62.herokuapp.com',
