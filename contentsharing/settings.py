031e8739abb65a22afeda9d3b08f73edbc67b0b0
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-07-10 19:17:08 +0000

    fixing settings

--- contentsharing/settings.py
+++ contentsharing/settings.py
@@ -171,7 +171,7 @@ if not DEBUG:
     ]
 
 REST_USE_JWT = True
-JWT_AUTH_SECURE = True
+JWT_AUTH_SECURE = not DEBUG  # Ensure this matches your environment
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
