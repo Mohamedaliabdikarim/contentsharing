0e9f9549ce306ab52c9a43971b9f5067bb7b7634
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-07-10 21:56:30 +0000

    added new view

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from posts import views
 urlpatterns = [
     path('posts/', views.PostList.as_view(), name='post-list'),
     path('posts/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
+    path('posts/category/<str:category_name>/', views.PostByCategoryList.as_view(), name='posts-by-category'),
     path('categories/', views.CategoryList.as_view(), name='category-list'),
     path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,25 +1,23 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.response import Response
+from contentsharing.permissions import IsOwnerOrReadOnly
 from .models import Post, Category
 from .serializers import PostSerializer, CategorySerializer
-from contentsharing.permissions import IsOwnerOrReadOnly
 
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response({"results": serializer.data})
 
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 class PostList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.annotate(
@@ -53,9 +51,27 @@ class PostList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.annotate(
         likes_count=Count('likes', distinct=True),
         comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
+
+
+class PostByCategoryList(generics.ListAPIView):
+    """
+    List posts filtered by category
+    """
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        category_name = self.kwargs['category_name']
+        return Post.objects.filter(category__name=category_name).annotate(
+            likes_count=Count('likes', distinct=True),
+            comments_count=Count('comment', distinct=True)
+        ).order_by('-created_at')
