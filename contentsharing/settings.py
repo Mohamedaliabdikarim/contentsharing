e08b3d8fb9a551b27b8991f4e163b550a4c88411
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-07-10 18:12:44 +0000

    fixing settings

--- contentsharing/settings.py
+++ contentsharing/settings.py
@@ -156,10 +156,6 @@ REST_AUTH_SERIALIZERS = {
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
-)
 
 @api_view()
 def root_route(request):
@@ -190,7 +186,3 @@ def logout_route(request):
         secure=JWT_AUTH_SECURE,
     )
     return response
-
-    
-
-
