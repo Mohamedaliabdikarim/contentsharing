6b156bb3cdfeb6c83dc66812556a5b7f8a1cd433
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-07-15 12:40:20 +0000

    impore re to settings

--- contentsharing/settings.py
+++ contentsharing/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import dj_database_url
+import re
 
 # Import the env.py file if it exists
 if os.path.exists('env.py'):
--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../ddefault_profile_vc7caf'
+        upload_to='images/', default='../default_profile_vc7caf'
     )
 
     class Meta:
